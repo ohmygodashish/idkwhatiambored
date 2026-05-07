99dfbd7ce37897663c0d5879a979d12bf66d3ad9
Ashish Prajapati <ashishanshprajapati@gmail.com>
2026-05-07 22:40:15 +0530

    feat: render Projects and Contact in App

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,15 @@
 import Hero from "./components/Hero";
 import About from "./components/About";
-import Features from "./components/Features";
+import Projects from "./components/Projects";
+import Contact from "./components/Contact";
 
 export default function App() {
   return (
     <main>
       <Hero />
       <About />
-      <Features />
+      <Projects />
+      <Contact />
     </main>
   );
 }
