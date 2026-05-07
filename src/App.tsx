8edaa81349384051796c92b92d93871c5748d6c3
Ashish Prajapati <ashishanshprajapati@gmail.com>
2026-05-08 04:57:18 +0530

    refactor: update section imports in App

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
-import Hero from "./components/Hero";
-import About from "./components/About";
-import Projects from "./components/Projects";
-import Contact from "./components/Contact";
+import Hero from "./sections/Hero";
+import About from "./sections/About";
+import Projects from "./sections/Projects";
+import Contact from "./sections/Contact";
 
 export default function App() {
   return (
